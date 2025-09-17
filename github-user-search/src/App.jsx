23301b236222d6730a5c7e56254d36c1cde511e6
Kingyanjui <alexkinyanjui14@gmail.com>
2025-09-17 17:15:53 +0300

    Initial React project setup for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>GitHub User Search Application</h1>
+      <p>Start building your app by adding components and services.</p>
+    </div>
+  );
+}
+
+export default App;
